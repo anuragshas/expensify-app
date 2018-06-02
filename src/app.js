1744e5a2d5828f4b254d881996e868d0759f6a81
anurag <anurag.comm@gmail.com>
2018-06-02 18:36:42 +0000

    completed dateFilers, added jest

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ store.subscribe(()=>{
 
 const expense_one = store.dispatch(addExpense({description:'Water bill',amount:100, createdAt:1200}));
 const expense_two = store.dispatch(addExpense({description:'Coffee bill',amount:5, createdAt:400}));
-store.dispatch(sortByAmount());
 
 const jsx = (
     <Provider store ={store}>
