9a1ba933303492060115e4fd9ff08a77e3c6b0d4
anurag <anurag.comm@gmail.com>
2018-06-03 03:54:09 +0000

    added tests for reducers

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,26 +1,6 @@
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
-
-const expenses = [{
-        id:'1',
-        description: 'Gun',
-        note:'',
-        amount:500,
-        createdAt:0
-    },{
-        id:'2',
-        description: 'Rifle',
-        note:'',
-        amount:1500,
-        createdAt:moment(0).subtract(4,'days').valueOf()
-    },{
-        id:'3',
-        description: 'Pistol',
-        note:'',
-        amount:800,
-        createdAt:moment(0).add(4,'days').valueOf()
-    }
-];
+import expenses from '../fixtures/expenses';
 
 
 test('should filter by text value',()=>{
