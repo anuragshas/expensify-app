d265d4d05b625f32d9a28f37f23ded65abd71fd2
anurag <anurag.comm@gmail.com>
2018-06-03 05:24:45 +0000

    setup expensify

--- src/app.js
+++ src/app.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import getVisibleExpenses from'./selectors/expenses';
-import {addExpense} from "./actions/expenses";
-import {setTextFilter, sortByAmount} from "./actions/filters";
+import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.subscribe(()=>{
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-    // console.log(visibleExpenses);
-});
-
-const expense_one = store.dispatch(addExpense({description:'Water bill',amount:100, createdAt:1200}));
-const expense_two = store.dispatch(addExpense({description:'Coffee bill',amount:5, createdAt:400}));
 
 const jsx = (
     <Provider store ={store}>
