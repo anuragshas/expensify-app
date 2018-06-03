32897965ca0866c9a1e9d928a2eb1de8803c8045
anurag <anurag.comm@gmail.com>
2018-06-03 21:20:05 +0000

    added notfound and dashboard tests

--- src/tests/components/ExpenseDashboardPage.test.js
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
 
-test('Should render NotFoundPage Correctly',()=>{
+test('Should render ExpenseDashboardPage Correctly',()=>{
     const wrapper = shallow(<ExpenseDashboardPage/>);
     expect(wrapper).toMatchSnapshot();
 });
