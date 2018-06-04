7686ef6bc7344e347521a5455372d3f394a45d6b
anurag <anurag.comm@gmail.com>
2018-06-04 04:21:20 +0000

    completed add expense summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -19,7 +19,7 @@ const mapStateToProps =(state)=> {
     const visibleExpenses = selectExpenses(state.expenses,state.filters);
     return{
         expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(state.expenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     };
 };
 
