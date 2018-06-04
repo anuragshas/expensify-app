4e7b84c32fd022feb451eaddb14f0e6677a73b66
anurag <anurag.comm@gmail.com>
2018-06-04 04:26:33 +0000

    completed add expense summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -9,7 +9,7 @@ export const ExpensesSummary = ({expenseCount,expensesTotal})=>{
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
         <div>
-            {expenseCount!==0 && <p>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal} </p>}
+            {expenseCount!==0 && <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal} </h1>}
         </div>
     );
 
